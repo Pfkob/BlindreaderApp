e458a58312751c90b6716d97dece47cced53fbbb
Piangfa Boonkaew <119128956+Pfkob@users.noreply.github.com>
2024-01-25 12:21:21 +0700

    Add new file main.dart and screencapture

---
+++ main.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/widgets.dart';
+
+main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 60.0,
+          horizontal: 80.0,
+        ),
+        color: Color(0xFFFFFFFF),
+        child: Content(),
+      ),
+    );
+  }
+}
+
+class Content extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Counter('6488201'),
+        Counter('Piangfa Boonkaew'),
+        Counter('Blindreader Application'),
+        Counter('028,080,201,214'),
+      ],
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  final String _name;
+  Counter(this._name);
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.0),
+      padding: EdgeInsets.all(4.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xFFFD6A02)),
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // [widget] is the property of the State class that stores
+          // the instance of the [StatefulWidget] ([Counter] in our case)
+          _CounterLabel(widget._name),
+          _CounterButton(
+            count,
+            onPressed: () {
+              setState(() {
+                ++count;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CounterLabel extends StatelessWidget {
+  static const textStyle = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 26.0,
+  );
+
+  final String _label;
+  _CounterLabel(this._label);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _label,
+      style: _CounterLabel.textStyle,
+    );
+  }
+}
+
+class _CounterButton extends StatelessWidget {
+  final count;
+  final onPressed;
+  _CounterButton(this.count, {@required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 6.0),
+        decoration: BoxDecoration(
+          color: Color(0xFFFD6A02),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Center(
+          child: Text(
+            '$count',
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+/*import 'package:flutter/material.dart';
+
+void main() {
+  var m = MyApp();
+  runApp(m);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var m = MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+    return m; //return widget
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+*/
